ece67ce37f0f3fa3c1c826fa9cfabd584a1cb220
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-17 19:08:08 -0600

    Targeted takeoff button and added event listener

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 window.addEventListener('load', function() {
-    
+    const takeOffBtn = document.querySelector('#takeoff')
+    takeOffBtn.addEventListener('click', function() {
+        confirm('Confirm that the shuttle is ready for take off.');
+    })
 })
