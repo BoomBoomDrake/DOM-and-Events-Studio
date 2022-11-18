be4e321dcedf4bfb204966d84223717524e03b76
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-17 20:30:46 -0600

    Down button event moves rocket image down 10px

--- scripts.js
+++ scripts.js
@@ -52,5 +52,6 @@ window.addEventListener('load', function() {
 
     downBtn.addEventListener('click', function() {
         spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
+        rocket.style.bottom = `${parseInt(rocket.style.bottom) - 10}px`;
     })
 })
