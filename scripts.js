b59103614dde4934802bff5734d22fbab6782495
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-17 20:02:37 -0600

    Targeted rocket image and set initial position

--- scripts.js
+++ scripts.js
@@ -3,6 +3,13 @@ window.addEventListener('load', function() {
     const flightStatus = document.querySelector('#flightStatus');
     const shuttleBackground = document.querySelector('#shuttleBackground');
     const spaceShuttleHeight = document.querySelector('#spaceShuttleHeight');
+    const rocket = document.querySelector('#rocket');
+    //Initial rocket position, centered.
+    rocket.style.position = 'absolute';
+    rocket.style.bottom = '0px';
+    rocket.style.left = '130px';
+
+    
 
     takeOffBtn.addEventListener('click', function() {
         if (confirm('Confirm that the shuttle is ready for take off.')) {
