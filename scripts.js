cec023f4404532199e79404780c46adf1d2e8c99
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-17 19:30:36 -0600

    Targeted spaceShuttleHeight text and added 10000 during takeOffBtn event

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 window.addEventListener('load', function() {
     const takeOffBtn = document.querySelector('#takeoff');
     const flightStatus = document.querySelector('#flightStatus');
+    const shuttleBackground = document.querySelector('#shuttleBackground');
+    const spaceShuttleHeight = document.querySelector('#spaceShuttleHeight');
 
     takeOffBtn.addEventListener('click', function() {
         if (confirm('Confirm that the shuttle is ready for take off.')) {
             flightStatus.innerHTML = `Shuttle in flight`;
+            shuttleBackground.style.backgroundColor = 'blue';
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
         }
     })
 })
